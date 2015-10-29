3a7c62d577eb0636f2eb4770866a19d49bd70230
UCSDMath <ucsdmath.project@gmail.com>
2015-10-29 13:42:09 -0700

    Update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,64 @@
-CHANGELOG=========All notable changes to `UCSDMath\Sftp` (bug and security fixes) willbe documented in this file.# 1.4.0 (2015-10-12) - Updated DDL database schema for system_logs, office_hours, ta_assignments - Updated comments to Entity and Maps for use with Persistance class. - Various javascript updates - Added multiple validation methods found in Validation class - Corrected and updated method in Pagination - other items were done...# 1.3.0 (2015-09-29) - New use of ORM and class called Persistence - Created 38 new Entity and maps for testing database tables. - Added Doctrine ORM and connections using ConfigurationVault - Javascript updates - Updated some logging output for various applications# 1.2.0 (2015-09-10) - Updated core javascript - Added boolean AND as default for word spacing all applications - Updated AbstractDatabase to work with DDL/SQL Updates - Structural updates various# 1.1.0 (2015-08-07) - Updated core javascript - Added boolean AND as default for word spacing - Added new class component Pagination - Minor configuration changes# 1.0.5 (2015-07-20) - Fixed some smaller issues with quick searches - Added Booleans AND OR NOT to quick searches - now uses full-text search# 1.0.4 (2015-07-14) - Added and updated many methods to Pdf class# 1.0.3 (2015-07-05) - Check for existing autoloader or notify - Autoload using absolute system path - Removed superglobals $_GET, $_POST, $_SERVER from codebase# 1.0.2 (2015-06-30) - Altered paths to allow local environment.# 1.0.1 (2015-06-29) - Fixed warning on "Ambiguous class resolution" when dumping optimized autoloader# 1.0.0 (2015-06-29) - Initial Release of `UCSDMath\Sftp`
+CHANGELOG
+=========
+
+All notable changes to `UCSDMath\Sftp` (bug and security fixes) will
+be documented in this file.
+
+# 1.4.0 (2015-10-12)
+
+ - Updated DDL database schema for system_logs, office_hours, ta_assignments
+ - Updated comments to Entity and Maps for use with Persistance class.
+ - Various javascript updates
+ - Added multiple validation methods found in Validation class
+ - Corrected and updated method in Pagination
+ - other items were done...
+
+# 1.3.0 (2015-09-29)
+
+ - New use of ORM and class called Persistence
+ - Created 38 new Entity and maps for testing database tables.
+ - Added Doctrine ORM and connections using ConfigurationVault
+ - Javascript updates
+ - Updated some logging output for various applications
+
+# 1.2.0 (2015-09-10)
+
+ - Updated core javascript
+ - Added boolean AND as default for word spacing all applications
+ - Updated AbstractDatabase to work with DDL/SQL Updates
+ - Structural updates various
+
+# 1.1.0 (2015-08-07)
+
+ - Updated core javascript
+ - Added boolean AND as default for word spacing
+ - Added new class component Pagination
+ - Minor configuration changes
+
+# 1.0.5 (2015-07-20)
+
+ - Fixed some smaller issues with quick searches
+ - Added Booleans AND OR NOT to quick searches
+ - now uses full-text search
+
+# 1.0.4 (2015-07-14)
+
+ - Added and updated many methods to Pdf class
+
+# 1.0.3 (2015-07-05)
+
+ - Check for existing autoloader or notify
+ - Autoload using absolute system path
+ - Removed superglobals $_GET, $_POST, $_SERVER from codebase
+
+# 1.0.2 (2015-06-30)
+
+ - Altered paths to allow local environment.
+
+# 1.0.1 (2015-06-29)
+
+ - Fixed warning on "Ambiguous class resolution" when dumping optimized autoloader
+
+# 1.0.0 (2015-06-29)
+
+ - Initial Release of `UCSDMath\Sftp`
--- src/Sftp/Sftp.php
+++ src/Sftp/Sftp.php
@@ -1,1 +1,226 @@
-<?php/* * This file is part of the UCSDMath package. * * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu> * * For the full copyright and license information, please view the LICENSE * file that was distributed with this source code. */namespace UCSDMath\Sftp;/** * Sftp is the default implementation of {@link SftpInterface} which * provides routine sftp methods that are commonly used throughout the framework. * * Method list: * * @method SftpInterface __construct(); * @method void __destruct(); * @method getPath(); * @method renameFile($absolutePath_old, $absolutePath_new); * @method renameDirectory($absolutePath_old, $absolutePath_new); * @method touch($absolutePath); * @method uploadString($absolutePath_remoteFile, $str); * @method downloadString($absolutePath_remoteFile); * * @author Daryl Eisner <deisner@ucsd.edu> * * @api */class Sftp extends AbstractSftp implements SftpInterface{    /**     * Constants.     *     * @var string VERSION  A version number     *     * @api     */    const VERSION = '1.4.0';    // --------------------------------------------------------------------------    /**     * Properties.     */    // --------------------------------------------------------------------------    /**     * Constructor.     *     * @api     */    public function __construct()    {        parent::__construct();    }    // --------------------------------------------------------------------------    /**     * Get directory path.     *     * @return string     *     * @api     */    public function getPath()    {        return $this->sftp->pwd();    }    // --------------------------------------------------------------------------    /**     * Rename a file or directory.     *     * @param  string $absolutePath_old  A absolute path to file (in old name)     * @param  string $absolutePath_new  A absolute path to file (in new name)     *     * @api     */    public function renameFile($absolutePath_old, $absolutePath_new)    {        $this->sftp->rename($absolutePath_old, $absolutePath_new);        return $this;    }    // --------------------------------------------------------------------------    /**     * Rename a file or directory.     *     * @param  string $absolutePath_old  A absolute path to file (in old name)     * @param  string $absolutePath_new  A absolute path to file (in new name)     *     * @api     */    public function renameDirectory($absolutePath_old, $absolutePath_new)    {        $this->sftp->rename($absolutePath_old, $absolutePath_new);        return $this;    }    // --------------------------------------------------------------------------    /**     * List all files in a directory.     *     * @internal this is like '/bin/ls' on unix, but returned as array.     *     * @param  string $absolutePath  A directory name preference     *     * @return array     *     * @throws \throwInvalidArgumentExceptionError if $absolutePath is not a string/null     *     * @api     */    public function ls($absolutePath = null)    {        if (! is_null($absolutePath)) {            $theOldDirectoryPath = $this->pwd();            $this->changeToDirectory($absolutePath);            $theDirectoryFiles = $this->sftp->nlist();            $this->changeToDirectory($theOldDirectoryPath);        } else {            $theDirectoryFiles = $this->sftp->nlist();        }        return $theDirectoryFiles;    }    // --------------------------------------------------------------------------    /**     * Return specific file information on remote host.     *     * @return array     *     * @api     */    public function stat($remoteFileName)    {        return $this->sftp->stat($remoteFileName);    }    // --------------------------------------------------------------------------    /**     * Return specific file information on remote host.     *     * @return array     *     * @api     */    public function lstat($remoteFileName)    {        return $this->sftp->lstat($remoteFileName);    }    // --------------------------------------------------------------------------    /**     * Update the access/modification date of a file or directory (touch).     *     * @param  string $absolutePath  A relative or absolute filename     *     * @return int|bool  (bool on error)     *     * @throws \throwInvalidArgumentExceptionError if $remoteFileName is not a defined string     *     * @api     */    public function touch($absolutePath)    {        $this->sftp->touch($absolutePath);        return $this;    }    // --------------------------------------------------------------------------    /**     * Upload string-to-file.     *     * @param  string $absolutePath_remoteFile  A absolute path to remote file (new)     * @param  string $str                      A variable string     *     * @return SftpInterface     *     * @api     */    public function uploadString($absolutePath_remoteFile, $str)    {        $this->sftp->put($absolutePath_remoteFile, $str);        return $this;    }    // --------------------------------------------------------------------------    /**     * Download file-to-string.     *     * @param  string $absolutePath_remoteFile  A absolute path to remote file     *     * @return SftpInterface     *     * @api     */    public function downloadString($absolutePath_remoteFile)    {        return $this->sftp->get($absolutePath_remoteFile);    }}
+<?php
+
+/*
+ * This file is part of the UCSDMath package.
+ *
+ * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace UCSDMath\Sftp;
+
+/**
+ * Sftp is the default implementation of {@link SftpInterface} which
+ * provides routine sftp methods that are commonly used throughout the framework.
+ *
+ * Method list:
+ *
+ * @method SftpInterface __construct();
+ * @method void __destruct();
+ * @method getPath();
+ * @method renameFile($absolutePath_old, $absolutePath_new);
+ * @method renameDirectory($absolutePath_old, $absolutePath_new);
+ * @method touch($absolutePath);
+ * @method uploadString($absolutePath_remoteFile, $str);
+ * @method downloadString($absolutePath_remoteFile);
+ *
+ * @author Daryl Eisner <deisner@ucsd.edu>
+ *
+ * @api
+ */
+class Sftp extends AbstractSftp implements SftpInterface
+{
+    /**
+     * Constants.
+     *
+     * @var string VERSION  A version number
+     *
+     * @api
+     */
+    const VERSION = '1.4.0';
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Properties.
+     */
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Constructor.
+     *
+     * @api
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Get directory path.
+     *
+     * @return string
+     *
+     * @api
+     */
+    public function getPath()
+    {
+        return $this->sftp->pwd();
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Rename a file or directory.
+     *
+     * @param  string $absolutePath_old  A absolute path to file (in old name)
+     * @param  string $absolutePath_new  A absolute path to file (in new name)
+     *
+     * @api
+     */
+    public function renameFile($absolutePath_old, $absolutePath_new)
+    {
+        $this->sftp->rename($absolutePath_old, $absolutePath_new);
+
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Rename a file or directory.
+     *
+     * @param  string $absolutePath_old  A absolute path to file (in old name)
+     * @param  string $absolutePath_new  A absolute path to file (in new name)
+     *
+     * @api
+     */
+    public function renameDirectory($absolutePath_old, $absolutePath_new)
+    {
+        $this->sftp->rename($absolutePath_old, $absolutePath_new);
+
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * List all files in a directory.
+     *
+     * @internal this is like '/bin/ls' on unix, but returned as array.
+     *
+     * @param  string $absolutePath  A directory name preference
+     *
+     * @return array
+     *
+     * @throws \throwInvalidArgumentExceptionError if $absolutePath is not a string/null
+     *
+     * @api
+     */
+    public function ls($absolutePath = null)
+    {
+        if (! is_null($absolutePath)) {
+            $theOldDirectoryPath = $this->pwd();
+            $this->changeToDirectory($absolutePath);
+
+            $theDirectoryFiles = $this->sftp->nlist();
+            $this->changeToDirectory($theOldDirectoryPath);
+
+        } else {
+            $theDirectoryFiles = $this->sftp->nlist();
+        }
+
+        return $theDirectoryFiles;
+
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Return specific file information on remote host.
+     *
+     * @return array
+     *
+     * @api
+     */
+    public function stat($remoteFileName)
+    {
+
+        return $this->sftp->stat($remoteFileName);
+
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Return specific file information on remote host.
+     *
+     * @return array
+     *
+     * @api
+     */
+    public function lstat($remoteFileName)
+    {
+        return $this->sftp->lstat($remoteFileName);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Update the access/modification date of a file or directory (touch).
+     *
+     * @param  string $absolutePath  A relative or absolute filename
+     *
+     * @return int|bool  (bool on error)
+     *
+     * @throws \throwInvalidArgumentExceptionError if $remoteFileName is not a defined string
+     *
+     * @api
+     */
+    public function touch($absolutePath)
+    {
+        $this->sftp->touch($absolutePath);
+
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Upload string-to-file.
+     *
+     * @param  string $absolutePath_remoteFile  A absolute path to remote file (new)
+     * @param  string $str                      A variable string
+     *
+     * @return SftpInterface
+     *
+     * @api
+     */
+    public function uploadString($absolutePath_remoteFile, $str)
+    {
+        $this->sftp->put($absolutePath_remoteFile, $str);
+
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Download file-to-string.
+     *
+     * @param  string $absolutePath_remoteFile  A absolute path to remote file
+     *
+     * @return SftpInterface
+     *
+     * @api
+     */
+    public function downloadString($absolutePath_remoteFile)
+    {
+        return $this->sftp->get($absolutePath_remoteFile);
+    }
+}
