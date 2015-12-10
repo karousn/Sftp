994dafbc173b8560ee0cca8a3ad1c52670db505f
UCSDMath <ucsdmath.project@gmail.com>
2015-12-10 10:31:55 -0800

    Update 1.5.x-dev

--- src/Sftp/SftpInterface.php
+++ src/Sftp/SftpInterface.php
@@ -1,1 +1,29 @@
-<?php/* * This file is part of the UCSDMath package. * * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu> * * For the full copyright and license information, please view the LICENSE * file that was distributed with this source code. */namespace UCSDMath\Sftp;/** * SftpInterface is the interface implemented by all Sftp classes. * * @author Daryl Eisner <deisner@ucsd.edu> */interface SftpInterface{    /**     * Constants.     */    const REQUIRED_PHP_VERSION = '5.6.10';    const DEFAULT_CHARSET = 'UTF-8';    const SOURCE_LOCAL_FILE = 1;    const SOURCE_CALLBACK = 16;    const SOURCE_STRING = 2;}
+<?php
+
+/*
+ * This file is part of the UCSDMath package.
+ *
+ * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace UCSDMath\Sftp;
+
+/**
+ * SftpInterface is the interface implemented by all Sftp classes.
+ *
+ * @author Daryl Eisner <deisner@ucsd.edu>
+ */
+interface SftpInterface
+{
+    /**
+     * Constants.
+     */
+    const REQUIRED_PHP_VERSION = '5.6.10';
+    const DEFAULT_CHARSET = 'UTF-8';
+    const SOURCE_LOCAL_FILE = 1;
+    const SOURCE_CALLBACK = 16;
+    const SOURCE_STRING = 2;
+}
