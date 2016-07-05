fda5e34b9f6b2b90922aaa59cdc0e0cac8ab4e58
Daryl Eisner <ucsdmath.project@gmail.com>
2016-07-05 15:11:41 -0700

    Update

--- src/Sftp/SftpInterface.php
+++ src/Sftp/SftpInterface.php
@@ -26,12 +26,24 @@ interface SftpInterface
 {
     /**
      * Constants.
+     *
+     * @var string FRAMEWORK_MINIMUM_PHP The framework's minimum supported PHP version
+     * @var string DEFAULT_CHARSET       The character encoding for the system
+     * @var string CRLF                  The carriage return line feed
+     * @var bool   REQUIRE_HTTPS         The secure setting TLS/SSL site requirement
+     * @var string DEFAULT_TIMEZONE      The local timezone for the server (or set in ini.php)
+     * @var int    SOURCE_LOCAL_FILE     The common stream source as local file (type)
+     * @var int    SOURCE_CALLBACK       The common stream source as a callback (type)
+     * @var int    SOURCE_STRING         The common stream source as inline string (type)
      */
-    const REQUIRED_PHP_VERSION = '7.0.0';
-    const DEFAULT_CHARSET = 'UTF-8';
-    const SOURCE_LOCAL_FILE = 1;
-    const SOURCE_CALLBACK = 16;
-    const SOURCE_STRING = 2;
+    const FRAMEWORK_MINIMUM_PHP = '7.0.0';
+    const DEFAULT_CHARSET       = 'UTF-8';
+    const CRLF                  = "\r\n";
+    const REQUIRE_HTTPS         = true;
+    const DEFAULT_TIMEZONE      = 'America/Los_Angeles';
+    const SOURCE_LOCAL_FILE     = 1;
+    const SOURCE_CALLBACK       = 16;
+    const SOURCE_STRING         = 2;
 
     //--------------------------------------------------------------------------
 }
