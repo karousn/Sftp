d64a4acce43031e60dbadcb0e5ac056a9096a8e2
UCSDMath <UCSDMath.Project@gmail.com>
2017-02-14 10:50:49 -0800

    Update

--- src/Sftp/AbstractSftp.php
+++ src/Sftp/AbstractSftp.php
@@ -55,7 +55,7 @@ abstract class AbstractSftp implements SftpInterface, ServiceFunctionsInterface,
      *
      * @api
      */
-    public const VERSION = '1.14.0';
+    public const VERSION = '1.15.0';
 
     //--------------------------------------------------------------------------
 
